846dbb62e219f42eb1166d861cccab5820f787c1
Sean Boyle <sean94@gmail.com>
2020-07-13 20:41:34 +0100

    Added firebase to details to the project

--- client/src/components/Posts/PostForm.js
+++ client/src/components/Posts/PostForm.js
@@ -1,65 +1,81 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class PostForm extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      text: '',
+      text: "",
       post_img: null,
-      postImgURL: '',
-      longitude: '',
-      latitude: '',
-      feed_id: '',
-    }
+      postImgURL: "",
+      longitude: "",
+      latitude: "",
+      feed_id: ""
+    };
   }
 
   handlePostCreate = () => {
-    console.log(this.props.auth.searchedUser)
-    let { searchedUser, user } = this.props.auth
+    console.log(this.props.auth.searchedUser);
+    console.log(this.state);
+
+    let { text, post_img } = this.state;
+    let { searchedUser, user } = this.props.auth;
+    let { feed_id } = this.props;
     navigator.geolocation.getCurrentPosition(
-      (position) => {
-        let { latitude, longitude } = position.coords
+      position => {
+        let { latitude, longitude } = position.coords;
         this.setState({
-          latitude,
-          longitude,
-          feed_id: searchedUser._id,
-        })
+          latitude: latitude,
+          longitude: longitude,
+          feed_id: feed_id,
+          text
+        });
       },
-      (error) => {
-        this.props.displayError('Error dectecting your location')
-        console.error(JSON.stringify(error))
+      error => {
+        this.props.displayError("Error dectecting your location");
+        console.error(JSON.stringify(error));
       },
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    )
+    );
 
-    let createPostData = {
-      text: this.state.text,
-      location: {},
-    }
-  }
-  handleInputChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
+    // let createPostData = {
+    //   text: this.state.text,
+    //   location: {}
+    // };
+    // console.log(this.state);
+  };
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
   render() {
     // console.log(this.state)
 
     return (
       <div>
         <h1>POst Form</h1>
-        <button className='btn btn-primary' onClick={this.handlePostCreate}>
+        <input
+          type="text"
+          name="text"
+          value={this.state.text}
+          placeholder="Post text"
+          onChange={this.handleInputChange}
+        />
+        <button className="btn btn-primary" onClick={this.handlePostCreate}>
           Handle Post Create
         </button>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-})
+const mapStateToProps = state => ({
+  auth: state.auth
+});
 
-const mapDispatchToProps = {}
+const mapDispatchToProps = {};
 
-export default connect(mapStateToProps, mapDispatchToProps)(PostForm)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PostForm);
